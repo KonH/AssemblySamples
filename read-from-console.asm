5d5fa6b42594933d87683f9147dcd8a6bc34185a
KonH <konh@yandex.ru>
2020-07-05 00:30:44 +0700

    Modify console read behaviour and fix leaking input issue

--- read-from-console.asm
+++ read-from-console.asm
@@ -1,4 +1,4 @@
-; Read single character (and \n) from terminal input and write it back
+; Read all input from STDIN and represent it in STDOUT until empty input provided
 
 %define SYS_read 0x2000003
 %define SYS_write 0x2000004
@@ -9,15 +9,26 @@
 global _main ; Entry point
 
 section .bss
-inputBuffer resb 2
+inputBuffer resb 16
 inputBufferSize equ $-inputBuffer
 
 section .text
 
 _main:
-    call read_input
-    call write_input
+    call read_write_loop
     call exit
+    ret
+
+; To properly handle read process it's required to utilize all peding data
+; So read all chunks until empty input found
+read_write_loop:
+    .while_loop_body:
+        call read_input    ; RAX contains SYS_read return value,
+        cmp rax, 1         ; in case of 1, then no input provided (\0 only)
+        je .while_loop_end ; and we can finish
+        call write_input
+        jmp .while_loop_body
+    .while_loop_end:
     ret
 
 read_input:
